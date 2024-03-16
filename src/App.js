383d7fc4dd7f26b8bed158e10063fdc695337138
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2024-03-16 15:56:54 +0530

    Structure Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,82 @@
-import React from 'react'
-import './App.css'
-const App = () => {
-  return (
-    <div className='text-3xl underline'>
-      App
-    </div>
-  )
-}
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings, FiSidebar } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
-export default App
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
+import {Stacked,Orders,Calendar,Employees,Pyramid,Customers,Kanban,Area,Bar, Pie,Financial,ColorPicker,ColorMapping,Editor, Ecommercee} from "./pages";
+import "./App.css";
+const App = () => {
+  const activeMenu = false;
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="top">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                style={{ background: "blue", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className="w-72 bg-white fixed sidebar dark:bg-secondary-dark-bg ">
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0 bg-secondary-dark-bg"><Sidebar /></div>
+          )}
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            }`}
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              <Navbar />
+            </div>
+          </div>
+
+          <div>
+            <Routes>
+              {/* Dashboard */}
+
+              <Route path="/" element={<Ecommercee />} />
+              <Route path="/ecommerce" element={<Ecommercee />} />
+
+              {/* Pages */}
+
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/employees" element={<Employees />} />
+              <Route path="/customers" element={<Customers />} />
+
+              {/* Apps */}
+
+              <Route path="/kanban" element={<Kanban />} />
+              <Route path="/editor" element={<Editor />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/color-picker" element={<ColorPicker />} />
+
+              {/* Charts */}
+
+              <Route path="/line" element={<Line />} />
+              <Route path="/area" element={<Area />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/financial" element={<Financial />} />
+              <Route path="/color-mapping" element={<ColorMapping />} />
+              <Route path="/pyramid" element={<Pyramid />} />
+              <Route path="/stacked" element={<Stacked />} />
+
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Ecommercee.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Ecommercee = () => {
+  return (
+    <div>
+      Ecommercee
+    </div>
+  )
+}
+
+export default Ecommercee
