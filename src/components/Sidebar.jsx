9222a533781c1b87fc9e2e6b874fd4c1cdb1d411
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2024-03-18 00:13:37 +0530

    Add Navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,12 @@ import { links } from "../data/dummy";
 import { useStateContext } from "../context/ContextProvider";
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu,screenSize } = useStateContext();
+  const handleCloseSidebar = ()=>{
+    if(activeMenu && screenSize<=900){
+      setActiveMenu(false);
+    }
+  }
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
   const normalLink =
@@ -19,7 +24,7 @@ const Sidebar = () => {
           <div className="flex justify-between items-center ">
             <Link
               to="/"
-              onClick={() => setActiveMenu(false)}
+              onClick={handleCloseSidebar}
               className="flex items-center gap-3 ml-3 mt-4 text-xl font-extrabold dark:text-white text-slate-900 tracking-tight"
             >
               <SiShopware />
@@ -45,7 +50,7 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${Link.name}`}
                     key={Link.name}
-                    onClick={() => {}}
+                    onClick={handleCloseSidebar}
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
